e9b424fd2e42c706a239977b809f797d2bbfc1d1
RandomPerson1907 <random.person.1907@gmail.com>
2019-11-20 23:54:00 +0200

    Started styling login page

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="/css/admin/app.css">
+</head>
+<body>
+    <div id="app">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <form action="#" class="form">
+                        <div class="form__logo">
+                            <img src="#" alt="Logo">
+                        </div>
+                        <div class="form__input form__input_username">
+                            <input type="text" placeholder="Username">
+                        </div>
+                        <div class="form__input form__input_password">
+                            <input type="password" placeholder="Password">
+                            <button>Show password</button>
+                        </div>
+                        <div class="form__block d-flex">
+                            <div class="form__remember-me">
+                                <input id="form__remember-me__checkbox" class="d-none" type="checkbox">
+                                <span class="form__remember-me__icon">Icon</span>
+                                <label for="form__remember-me__checkbox" class="form__remember-me__label">Remember me</label>
+                            </div>
+                            <div class="form__forgot-password">
+                                <a href="#" class="form__forgot-password__link">Forgot password</a>
+                            </div>
+                        </div>
+                        <div class="form__submit d-flex justify-content-center">
+                            <button type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .sass('resources/sass/admin/app.scss', 'public/css/admin')
+    .js('resources/js/admin/app.js', 'public/js/admin')
