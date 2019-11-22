f349a4a9788cb602c622eb92a9e303b14475feff
RandomPerson1907 <random.person.1907@gmail.com>
2019-11-22 02:09:58 +0200

    Added styles to login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -11,8 +11,8 @@
 <body>
     <div id="app">
         <div class="container">
-            <div class="row">
-                <div class="col-md-4">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
                     <form action="#" class="form">
                         <div class="form__logo">
                             <img src="#" alt="Logo">
@@ -22,16 +22,21 @@
                         </div>
                         <div class="form__input form__input_password">
                             <input type="password" placeholder="Password">
-                            <button>Show password</button>
+                            <button class="form__show-password">
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                                     width="50" height="50"
+                                     viewBox="0 0 172 172"
+                                     style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M65.145,44.1825c-34.45375,9.29875 -59.59531,38.10875 -60.845,39.56c-1.10187,1.29 -1.10187,3.225 0,4.515c1.49156,1.73344 36.93969,42.4625 81.7,42.4625c44.76031,0 80.20844,-40.72906 81.7,-42.4625c1.10188,-1.29 1.10188,-3.225 0,-4.515c-1.24969,-1.45125 -26.39125,-30.24781 -60.845,-39.56c10.19906,6.7725 16.985,18.32875 16.985,31.4975c-0.01344,20.89531 -16.94469,37.84 -37.84,37.84c-20.89531,0 -37.82656,-16.94469 -37.84,-37.84c0,-13.16875 6.78594,-24.725 16.985,-31.4975zM86,58.48c-9.50031,0 -17.2,7.69969 -17.2,17.2c0,9.50031 7.69969,17.2 17.2,17.2c9.50031,0 17.2,-7.69969 17.2,-17.2c0,-9.50031 -7.69969,-17.2 -17.2,-17.2z"></path></g></g></svg>
+                            </button>
                         </div>
-                        <div class="form__block d-flex">
+                        <div class="form__block">
                             <div class="form__remember-me">
-                                <input id="form__remember-me__checkbox" class="d-none" type="checkbox">
-                                <span class="form__remember-me__icon">Icon</span>
+                                <input id="form__remember-me__checkbox" class="d-none form__remember-me__checkbox" type="checkbox">
+                                <span class="form__remember-me__icon"></span>
                                 <label for="form__remember-me__checkbox" class="form__remember-me__label">Remember me</label>
                             </div>
                             <div class="form__forgot-password">
-                                <a href="#" class="form__forgot-password__link">Forgot password</a>
+                                <a href="#" class="form__forgot-password__link">Forgot password?</a>
                             </div>
                         </div>
                         <div class="form__submit d-flex justify-content-center">
