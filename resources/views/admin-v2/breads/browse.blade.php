610e70302fcca8e62b9492d6b05334425a05cbe9
RandomPerson1907 <random.person.1907@gmail.com>
2019-12-12 23:39:59 +0200

    Started second version of dashboard

---
+++ resources/views/admin-v2/breads/browse.blade.php
@@ -0,0 +1,5 @@
+@extends("admin-v2.master")
+
+@section("content")
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, facere vitae! Harum autem laudantium corrupti, suscipit inventore sit mollitia molestias!</p>
+@stop
