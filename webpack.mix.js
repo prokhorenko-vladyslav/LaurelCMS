718ae94abcfcb995f5278b1ca1ffa83935048134
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-07-08 23:04:43 +0300

    Added styles for login page

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/admin/app.js', 'public/admin/js')
-    .sass('resources/sass/admin/app.scss', 'public/admin/css');
+    .sass('resources/sass/admin/app.scss', 'public/admin/css')
+    .copyDirectory('resources/img/admin', 'public/admin/img');
