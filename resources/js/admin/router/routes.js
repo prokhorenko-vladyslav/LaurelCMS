408a3a36bc98ca0ac19ca05f0a2dc591a424451f
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-07-12 22:37:59 +0300

    Added route for forgot password page

--- resources/js/admin/router/routes.js
+++ resources/js/admin/router/routes.js
@@ -1,6 +1,7 @@
 import Main from "../pages/Main";
 import Login from "../pages/auth/Login";
 import Lock from "../pages/auth/Lock";
+import Forgot from "../pages/auth/Forgot";
 
 export default [
     {
@@ -10,6 +11,11 @@ export default [
             {
                 path : 'login',
                 component : Login
+            },
+            {
+                path : 'forgot',
+                component : Forgot,
+                name : 'admin.auth.forgot'
             },
             {
                 path : 'lock',
