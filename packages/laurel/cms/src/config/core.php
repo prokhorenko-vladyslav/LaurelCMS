1e9d0b8da6405a64076215ee3f8cb42e25a444fc
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-08-01 01:19:32 +0300

    Added comment to core config file

--- packages/laurel/cms/src/config/core.php
+++ packages/laurel/cms/src/config/core.php
@@ -11,20 +11,20 @@
          * List of modules, which will be loaded, when CMS is running in the console
          */
         'console_modules' => [
-            'settings' => \Laurel\CMS\Modules\Settings\SettingsModule::class
+
         ],
 
         /**
          * List of modules, which will be loaded, when CMS loaded using HTTP Request
          */
         'http_modules' => [
-            'auth' => \Laurel\CMS\Modules\Auth\AuthModule::class
+
         ],
 
         /**
          * Aliases for overriding modules classes
          */
         'aliases' => [
-//            'settings' => \Laurel\CMS\Modules\Localization\LocalizationModule::class
+
         ]
     ];
