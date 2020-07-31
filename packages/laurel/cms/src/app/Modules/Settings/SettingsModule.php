6725428892eb301cb0f523477da226dcf5cda9f7
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-08-01 01:20:36 +0300

    Removed messages for testing methods from the modules classes

--- packages/laurel/cms/src/app/Modules/Settings/SettingsModule.php
+++ packages/laurel/cms/src/app/Modules/Settings/SettingsModule.php
@@ -9,21 +9,16 @@ class SettingsModule extends Module
 {
     public function load()
     {
-        dump('Settings loaded');
+
     }
 
     public function unload()
     {
-        dump('Settings unloaded');
+
     }
 
     public function canBeForgotten(): bool
     {
         return false;
-    }
-
-    public function get(string $settingAlias)
-    {
-        dd($settingAlias);
     }
 }
