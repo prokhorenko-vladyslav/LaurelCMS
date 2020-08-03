e011bec1a62e738610b0b6dd5173e56d7a1bf880
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-08-03 23:12:32 +0300

    Added logic to LocalizationModule to load all translations for group and single locale

---
+++ packages/laurel/cms/src/app/Modules/Localization/Config/localization.php
@@ -0,0 +1,9 @@
+<?php
+    return [
+        'default_locale' => config('app.locale', 'ru'),
+        'language_files_location' => resource_path('lang/'),
+        'locales' => [
+            'ru',
+            'en'
+        ]
+    ];
