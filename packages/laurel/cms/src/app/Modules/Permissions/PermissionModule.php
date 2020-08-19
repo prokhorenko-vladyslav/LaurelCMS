1da43ae10482f7794fb70deae994d99de58053bd
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-08-19 23:51:03 +0300

    Removed unused methods from Permission Module

--- packages/laurel/cms/src/app/Modules/Permissions/PermissionModule.php
+++ packages/laurel/cms/src/app/Modules/Permissions/PermissionModule.php
@@ -7,43 +7,5 @@ use Laurel\CMS\Abstracts\Module;
 
 class PermissionModule extends Module
 {
-    public function register()
-    {
-
-    }
-
-    public function login()
-    {
-
-    }
-
-    public function forgotPassword()
-    {
-
-    }
-
-    public function changePassword()
-    {
-
-    }
-
-    public function createToken()
-    {
-
-    }
-
-    public function updateToken()
-    {
-
-    }
-
-    public function createResetPasswordEmail()
-    {
-
-    }
-
-    public function sendResetPasswordEmail()
-    {
-
-    }
+    
 }
