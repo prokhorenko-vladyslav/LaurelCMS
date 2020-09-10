23381c989d5e175719b6f1c9ff35dd6a2586326e
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-09-11 01:06:18 +0300

    Updated users migrations and seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('second_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('second_name')->nullable();
             $table->string('login')->unique();
             $table->string('email')->unique();
             $table->enum('gender', [
                 \App\Models\User::MALE,
                 \App\Models\User::FEMALE,
-            ]);
+            ])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
